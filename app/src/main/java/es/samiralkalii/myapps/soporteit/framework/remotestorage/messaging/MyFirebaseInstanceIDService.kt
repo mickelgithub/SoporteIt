6b099fcb5e80c3cf56ec4f2c3403552e776aa254
administrador <blackman_0005@hotmail.com>
2019-11-13 23:19:19 +0100

    authentification

--- app/src/main/java/es/samiralkalii/myapps/soporteit/framework/remotestorage/messaging/MyFirebaseInstanceIDService.kt
+++ app/src/main/java/es/samiralkalii/myapps/soporteit/framework/remotestorage/messaging/MyFirebaseInstanceIDService.kt
@@ -1,11 +1,18 @@
 package es.samiralkalii.myapps.soporteit.framework.remotestorage.messaging
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import es.samiralkalii.myapps.usecase.messaging.RegisterMessagingTokenUseCase
 import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
 import org.slf4j.LoggerFactory
 import kotlin.coroutines.CoroutineContext
+
+private const val RESULT_OK_VALUE= "OK"
+private const val RESULT_KO_VALUE= "KO"
+private const val MESSAGE_ID_KEY= "messageId"
+private const val RESULT_KEY= "result"
+
 
 class MyFirebaseInstanceIDService() : CoroutineScope, FirebaseMessagingService() {
 
@@ -34,7 +41,16 @@ class MyFirebaseInstanceIDService() : CoroutineScope, FirebaseMessagingService()
                 sendRegistrationToServer(token)
             }.await()
         }
+    }
 
+    override fun onMessageReceived(remoteMsg: RemoteMessage) {
+        super.onMessageReceived(remoteMsg)
+        with(remoteMsg.data) {
+            val messageId= this[MESSAGE_ID_KEY] ?: ""
+            val result= this[RESULT_KEY] ?: ""
+            logger.debug("the message received is $messageId -> $result")
+
+        }
 
     }
 
