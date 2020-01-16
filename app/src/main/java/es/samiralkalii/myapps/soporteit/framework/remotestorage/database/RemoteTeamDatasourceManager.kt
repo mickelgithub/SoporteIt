aaeec1334d11eb07c7f46e865faa47168a9e15a8
administrador <blackman_0005@hotmail.com>
2020-01-16 23:20:49 +0100

    ---

--- app/src/main/java/es/samiralkalii/myapps/soporteit/framework/remotestorage/database/RemoteTeamDatasourceManager.kt
+++ app/src/main/java/es/samiralkalii/myapps/soporteit/framework/remotestorage/database/RemoteTeamDatasourceManager.kt
@@ -7,10 +7,7 @@ import es.samiralkalii.myapps.domain.User
 import es.samiralkalii.myapps.domain.notification.NotifType
 import es.samiralkalii.myapps.domain.notification.Notification
 import es.samiralkalii.myapps.domain.teammanagement.Team
-import es.samiralkalii.myapps.soporteit.ui.util.KEY_BOSS_VERIFICATION
-import es.samiralkalii.myapps.soporteit.ui.util.KEY_EMAIL_VERIFIED
-import es.samiralkalii.myapps.soporteit.ui.util.KEY_TEAM_ID
-import es.samiralkalii.myapps.soporteit.ui.util.KEY_TEAM_NAME_INSENSITIVE
+import es.samiralkalii.myapps.soporteit.ui.util.*
 import kotlinx.coroutines.tasks.await
 import org.slf4j.LoggerFactory
 
@@ -29,6 +26,7 @@ private const val VALUE_GROUP= "group"
 private const val TEAM_REF= "teams"
 private const val NOTIFS_SENT= "notifsSent"
 private const val NOTIFS_RECEIVED= "notifsReceived"
+private const val IN_PROGRESS= "in_progress"
 
 
 
@@ -77,11 +75,15 @@ class RemoteTeamDatasourceManager(val fstore: FirebaseFirestore): IRemoteTeamMan
             val notifRef= fstore.collection(NOTIFICATION_REF).document()
             //we add a notification to top level notifications
             notification.id= notifRef.id
-            val addTopLevelNotif= notifRef.set(notification.copy(deleted = null, deletionDate = null))
-            val addSentNotifsenderNotifRef= fstore.collection(USERS_REF).document(sender.id)
+            notifRef.set(notification.copy(deleted = null, deletionDate = null))
+            fstore.collection(USERS_REF).document(sender.id)
                 .collection(NOTIFS_SENT).document(notification.id!!).set(notification)
-            val addReceivedNotif= fstore.collection(USERS_REF).document(destination.id)
+            fstore.collection(USERS_REF).document(destination.id)
                 .collection(NOTIFS_RECEIVED).document(notification.id!!).set(notification)
+            fstore.collection(USERS_REF).document(destination.id).update(mapOf(
+                KEY_TEAM to IN_PROGRESS,
+                KEY_TEAM_ID to IN_PROGRESS
+            ))
 
         }.await()
 
