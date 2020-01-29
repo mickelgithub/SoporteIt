bccfbfbbeb1818ccfc6936da5f163f8784fd6e24
administrador <blackman_0005@hotmail.com>
2020-01-29 22:26:03 +0100

    ---

--- app/src/main/java/es/samiralkalii/myapps/soporteit/ui/home/notificactions/NotificationsFragment.kt
+++ app/src/main/java/es/samiralkalii/myapps/soporteit/ui/home/notificactions/NotificationsFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import es.samiralkalii.myapps.soporteit.R
 import es.samiralkalii.myapps.soporteit.databinding.FragmentNotificationsBinding
+import es.samiralkalii.myapps.soporteit.ui.home.HomeViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 import org.slf4j.LoggerFactory
 
 class NotificationsFragment: Fragment() {
@@ -14,6 +17,11 @@ class NotificationsFragment: Fragment() {
     private val logger= LoggerFactory.getLogger(NotificationsFragment::class.java)
 
     private lateinit var binding: FragmentNotificationsBinding
+
+    private val viewModel: NotificationsFragmentViewModel by viewModel()
+    private val homeViewModel: HomeViewModel by lazy {
+        ViewModelProviders.of(activity!!)[HomeViewModel::class.java]
+    }
 
     companion object {
         fun newInstance(bundle: Bundle) = NotificationsFragment().apply { arguments= bundle }
