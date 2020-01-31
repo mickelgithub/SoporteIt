06ae195552b71c3265d99eb7e27822a58edee2e9
ldasal4 <blackman_0005@hotmail.com>
2020-01-31 12:11:47 +0100

    ...

--- app/src/main/java/es/samiralkalii/myapps/soporteit/framework/remotestorage/database/RemoteNotificationsDatasourceManager.kt
+++ app/src/main/java/es/samiralkalii/myapps/soporteit/framework/remotestorage/database/RemoteNotificationsDatasourceManager.kt
@@ -4,11 +4,14 @@ import com.google.firebase.firestore.FirebaseFirestore
 import es.samiralkalii.myapps.data.notifications.IRemoteNotificationsDatasource
 import es.samiralkalii.myapps.domain.notification.Notification
 import kotlinx.coroutines.tasks.await
+import org.slf4j.LoggerFactory
 
 
 private const val KEY_NOTIFICATION_DELETED= "deleted"
 
 class RemoteNotificationsDatasourceManager(val fstore: FirebaseFirestore): IRemoteNotificationsDatasource {
+
+    private val logger= LoggerFactory.getLogger(RemoteNotificationsDatasourceManager::class.java)
 
     override suspend fun getNotificationReceivedById(
         userid: String,
@@ -21,7 +24,15 @@ class RemoteNotificationsDatasourceManager(val fstore: FirebaseFirestore): IRemo
     override suspend fun getReceivedNotifications(userid: String): List<Notification> {
         val result= fstore.collection(USERS_REF).document(userid).collection(NOTIFS_RECEIVED).whereEqualTo(
             KEY_NOTIFICATION_DELETED, false).get().await()
-        return result?.toObjects(List::class.java) as List<Notification>
+        //return result?.toObjects(List::class.java) as List<Notification>
+        //return result;
+        val mutableList= mutableListOf<Notification>()
+        for (document in result) {
+            val notif= document.toObject(Notification::class.java)
+            mutableList.add(notif)
+        }
+        logger.debug("el valor de resut ....")
+        return mutableList.toList()
     }
 
     override suspend fun getSentNotifications(userid: String): List<Notification> {
